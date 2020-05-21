76cc64d409a8cf758df2ecc91d0a8c4515f7a5a0
slsk3633 <slsk3633@gmail.com>
2020-05-21 14:57:07 +0900

    200521 tester complete

---
+++ tester/tester/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textbox_mul1 = new System.Windows.Forms.TextBox();
+            this.textbox_mul2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textbox_mul1
+            // 
+            this.textbox_mul1.Location = new System.Drawing.Point(26, 53);
+            this.textbox_mul1.Name = "textbox_mul1";
+            this.textbox_mul1.Size = new System.Drawing.Size(74, 21);
+            this.textbox_mul1.TabIndex = 1;
+            // 
+            // textbox_mul2
+            // 
+            this.textbox_mul2.Location = new System.Drawing.Point(106, 53);
+            this.textbox_mul2.Name = "textbox_mul2";
+            this.textbox_mul2.Size = new System.Drawing.Size(74, 21);
+            this.textbox_mul2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.button1.Location = new System.Drawing.Point(197, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "제곱";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "곱하기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(311, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(311, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 253);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textbox_mul2);
+            this.Controls.Add(this.textbox_mul1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "제곱과 곱하기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textbox_mul1;
+        private System.Windows.Forms.TextBox textbox_mul2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ tester/tester/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //case 1
+            new TestClass().Print();
+            //case 2(recommanded)
+            TestClass t = new TestClass();
+            t.Print();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int square = int.Parse(textBox1.Text);
+            TestClass test = new TestClass();
+            int result = test.Power(square);
+            label1.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int mul1 = int.Parse(textbox_mul1.Text);
+            int mul2 = int.Parse(textbox_mul2.Text);
+            TestClass test = new TestClass();
+            int result = test.Multi(mul1, mul2);
+            label2.Text = result.ToString();
+        }
+    }
+}
