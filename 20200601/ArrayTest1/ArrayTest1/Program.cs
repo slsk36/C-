f48a8e3c18278e3f5947b59441d79578aebc841d
slsk3633 <slsk3633@gmail.com>
2020-06-01 14:34:17 +0900

    200601 first commit

---
+++ 20200601/ArrayTest1/ArrayTest1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. 1차원 배열(number)
+            int[] arrInt = { 10,20,30,40,50 };
+            for(int i = 0; i < arrInt.Length; i++)
+            {
+                Console.WriteLine(arrInt[i] + " ");
+                string str = string.Format("배열[{0}] : {1}",
+                    i, arrInt[i]);
+                Console.WriteLine(str);
+            }
+            Console.WriteLine();
+
+            //2. 1차원 배열(string)
+            char[] arrCh = new char[5] { '이', '박', '김', '홍', '최' };
+            Console.WriteLine(arrCh);
+            Console.WriteLine();
+
+            //3. 빈공간 1차원 배열
+            string[] arrStr = new string[5];
+            Random rand = new Random();
+            string[] name = { "홍길동", "김길동", "최길동", "박길동", "이길동" };
+            for(int i = 0; i < arrStr.Length; i++)
+            {
+                //0~4까지의 랜덤한 숫자를 뽑는다.
+                int r = rand.Next(0, 5);
+                //비어있는 배열에다가 집어넣는다.
+                arrStr[i] = name[r];
+
+                Console.WriteLine(arrStr[i]);
+            }
+            Console.WriteLine();
+
+            //4. 2차원 배열 초기화
+            int[,] arr2Int =
+            {
+                {1,2,3}, {4,5,6 }
+            };
+
+            for(int i=0; i<2; i++) //고정(기준)
+            {
+                for(int j=0; j < 3; j++) //변동값
+                {
+                    Console.Write(arr2Int[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //5. 빈 공간 2차월 배열
+            string[][] arrStr2 = new string[2][];
+            arrStr2[0] = new string[] { "홍길동", "이길동", "박길동" };
+            arrStr2[1] = new string[] { "최길동" };
+
+            for(int i=0; i < arrStr2.Length; i++)
+            {
+                for(int j=0; j<arrStr2[0].Length; j++)
+                {
+                    Console.WriteLine(arrStr2[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ 20200601/consoleTest1/consoleTest1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleTest1
+{
+    //동전 교환 프로그램 만들기
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Coin500 = 0;
+            int Coin100 = 0;
+            int Coin50 = 0;
+            int Coin10 = 0;
+            int PaperMoney;
+            int Coin;
+
+            Console.Write("교환할 금액을 입력하세요");
+            PaperMoney = int.Parse(Console.ReadLine());
+            Console.WriteLine(PaperMoney);
+
+
+            //solution 1 : 동전 갯수를 직접입력 받아서 반환하기
+            //Console.Write("얼마짜리 동전이 필요하세요?");
+            //Coin = int.Parse(Console.ReadLine());
+
+            //switch (Coin)
+            //{
+            //    case 500:
+            //        Coin500 = PaperMoney / 500;
+            //        Console.WriteLine(Coin500 + "개 받으세요");
+            //        break;
+            //    case 100:
+            //        Coin100 = PaperMoney / 100;
+            //        Console.WriteLine(Coin100 + "개 받으세요");
+            //        break;
+            //    case 50:
+            //        Coin50 = PaperMoney / 50;
+            //        Console.WriteLine(Coin50 + "개 받으세요");
+            //        break;
+            //    case 10:
+            //        Coin10 = PaperMoney / 10;
+            //        Console.WriteLine(Coin10 + "개 받으세요");
+            //        break;
+            //    default:
+            //        Console.WriteLine("잘못된 값입니다.");
+            //        break;                   
+            //}
+            //Console.ReadKey();
+
+            //solution 2 : 여러가지 동전으로 반환하기
+
+            Coin500 = PaperMoney / 500;
+
+            PaperMoney = PaperMoney % 500;
+            Coin100 = PaperMoney / 100;
+
+            PaperMoney = PaperMoney % 100;
+            Coin50 = PaperMoney / 50;
+
+            PaperMoney = PaperMoney % 50;
+            Coin10 = PaperMoney / 10;
+
+            Coin = PaperMoney % 10;
+            
+
+            Console.WriteLine("500원: "+Coin500);
+            Console.WriteLine("100원: " + Coin100);
+            Console.WriteLine("50원: " + Coin50);
+            Console.WriteLine("10원: " + Coin10);
+            Console.WriteLine("나머지: " + Coin);
+
+
+
+
+
+        }
+    }
+}
